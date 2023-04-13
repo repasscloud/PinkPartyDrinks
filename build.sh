169be6cdcadec17459c38fbd74f2a3fbecbe875c
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2023-04-13 16:22:31 +1000

    save changes

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+dotnet publish PinkPartyDrinks.csproj -c Release -o release --nologo
+sed -i 's/<base href="\/" \/>/<base href="\/PinkPartyDrinks\/" \/>/g' release/wwwroot/index.html
+cp release/wwwroot/index.html release/wwwroot/404.html
+touch release/wwwroot/.nojekyll
